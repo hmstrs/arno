bad7f2fd0c43d20dc999d975c4241aa8044ed1a8
Khilchenko Yehor <khilchenko.yehor@gmail.com>
2020-01-19 15:24:53 +0200

    feat: Add Song model, modify User Model

---
+++ server/mongo/models/songModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const songSchema = new mongoose.Schema({
+  reference: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  artist: {
+    type: String,
+    required: true,
+  },
+  listened: {
+    type: Number,
+    required: true,
+  },
+  favourited: {
+    type: Number,
+    required: true
+  },
+});
+
+module.exports = mongoose.model('song', songSchema);
