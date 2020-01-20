f4f4514aed9e5c5b872ee40b4f962ab871e3c6f7
Khilchenko Yehor <khilchenko.yehor@gmail.com>
2020-01-20 22:30:14 +0200

    feat: modify recongniseByLirics method

--- server/graphql/resolvers/auddResolvers.js
+++ server/graphql/resolvers/auddResolvers.js
@@ -5,8 +5,8 @@ module.exports = {
   Query: {
     recogniseByLyrics: async (parent, { lyrics }, context, info) => {
       const responce = await fetch(
-        process.env.AUDD_LYRICS, 
-        { method: 'POST', body: { lyrics } }
+        `${process.env.AUDD_LYRICS}?api_token=${process.env.API_TOKEN}&q=${lyrics}`,
+        { method: 'POST' }
       ).then(res => res.json());
       return responce;
     },
