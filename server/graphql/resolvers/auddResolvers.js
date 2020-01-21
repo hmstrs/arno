691d8c2a9ad725a88fb336957a0f0b0c5fbcca8e
Khilchenko Yehor <khilchenko.yehor@gmail.com>
2020-01-21 17:14:21 +0200

    fix: change varibles names

--- server/graphql/resolvers/auddResolvers.js
+++ server/graphql/resolvers/auddResolvers.js
@@ -5,15 +5,15 @@ module.exports = {
   Query: {
     recogniseByLyrics: async (parent, { lyrics }, context, info) => {
       const responce = await fetch(
-        `${process.env.AUDD_LYRICS}?api_token=${process.env.API_TOKEN}&q=${lyrics}`,
+        `${process.env.AUDD_LYRICS}?api_token=${process.env.AUDD_API_TOKEN}&q=${lyrics}`,
         { method: 'POST' }
       ).then(res => res.json());
       return responce;
     },
     recogniseByHumming: async (parent, { humming }, context, info) => {
       const responce = await fetch(
-        `${process.env.AUDD_HUMMING}?api_token=${process.env.API_TOKEN}`, // add humming here
-        { method: 'POST', body: { humming } }
+        `${process.env.AUDD_HUMMING}?api_token=${process.env.AUDD_API_TOKEN}`, // add humming here
+        { method: 'POST' }
       ).then(res => res.json());
       return responce;
     },
