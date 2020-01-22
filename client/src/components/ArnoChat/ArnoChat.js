c3d8cc136f80c63da0d9bb1e9adb44d81d7d727f
Khilchenko Yehor <khilchenko.yehor@gmail.com>
2020-01-22 23:03:36 +0200

    keep changing layout

--- client/src/components/ArnoChat/ArnoChat.js
+++ client/src/components/ArnoChat/ArnoChat.js
@@ -5,11 +5,23 @@ import Logo from './logo.svg';
 const ArnoChat = ({ className }) => {
   return (
     <div className={"ArnoChat " + className}>
-      <img src={Logo} className="arno-logo"/>
-      <p className="main-text arno-text">тут говорит арно</p>
-      <p className="main-text user-text">тут говорит юзер</p>
-      <img src={Logo} className="arno-logo"/>
-      <p className="main-text arno-text">тут говорит арно уже больше слов, проверяю размер рамки</p>
+      <div className="chat-wrapper">
+        <img src={Logo} className="arno-logo"/>
+        <p className="main-text arno-text">тут говорит арно</p>
+        <p className="main-text user-text">тут говорит юзер</p>
+        <img src={Logo} className="arno-logo"/>
+        <p className="main-text arno-text">тут говорит арно уже больше слов, проверяю размер рамки</p>
+        <img src={Logo} className="arno-logo"/>
+        <p className="main-text arno-text">тут говорит арно</p>
+        <p className="main-text user-text">тут говорит юзер</p>
+        <img src={Logo} className="arno-logo"/>
+        <p className="main-text arno-text">тут говорит арно уже больше слов, проверяю размер рамки</p>
+      </div>
+      
+      <form className="bottom">
+        <input type="text" className="input" value="Lyrics here" name="chat" />
+        <button></button>
+      </form>
     </div>
   );
 };
