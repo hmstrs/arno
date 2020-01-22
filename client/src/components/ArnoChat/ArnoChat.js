8bcd380aa47d9181b91615b7562da654c8144687
Khilchenko Yehor <khilchenko.yehor@gmail.com>
2020-01-22 18:54:54 +0200

    add skeleton

--- client/src/components/ArnoChat/ArnoChat.js
+++ client/src/components/ArnoChat/ArnoChat.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './ArnoChat.css';
+import Logo from './logo.svg';
 
 const ArnoChat = ({className}) => {
   return (
     <div className={"ArnoChat " + className}>
-      <p className="main-text">тут чат с арно</p>
+      <img src={Logo} />
+      <p className="main-text arno-text">тут говорит арно</p>
+      <p className="main-text user-text">тут говорит юзер</p>
     </div>
   );
 };
