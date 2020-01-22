6eb53f8c90805c2792780e25aa5b8705a88dd9c3
Khilchenko Yehor <khilchenko.yehor@gmail.com>
2020-01-22 21:34:35 +0200

    feat: add prototype of chat

--- client/src/components/ArnoChat/ArnoChat.js
+++ client/src/components/ArnoChat/ArnoChat.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './ArnoChat.css';
 import Logo from './logo.svg';
 
-const ArnoChat = ({className}) => {
+const ArnoChat = ({ className }) => {
   return (
     <div className={"ArnoChat " + className}>
-      <img src={Logo} />
+      <img src={Logo} className="arno-logo"/>
       <p className="main-text arno-text">тут говорит арно</p>
       <p className="main-text user-text">тут говорит юзер</p>
+      <img src={Logo} className="arno-logo"/>
+      <p className="main-text arno-text">тут говорит арно уже больше слов, проверяю размер рамки</p>
     </div>
   );
 };
