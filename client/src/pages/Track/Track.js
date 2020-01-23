db8b932167bfef5243547921acac80b25bb4dea4
Khilchenko Yehor <khilchenko.yehor@gmail.com>
2020-01-24 00:42:41 +0200

    feat: add layout

--- client/src/pages/Track/Track.js
+++ client/src/pages/Track/Track.js
@@ -4,22 +4,33 @@ import { Col, Spinner } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import gql from "graphql-tag";
 
+import './Track.css';
+
 const GET_SONG = gql`
-	query song($id: ID!) {
-		song(id: $id) {
-			reference
-			title
-			artist
-			listened
-			favourited
-		}
-	}
+ query song($id: ID!) {
+  song(id: $id) {
+   reference
+   title
+   artist
+   listened
+   favourited
+  }
+ }
 `;
 
+const createButtons = (listened, favourited) => {
+  return (
+    <div class="buttons">
+      <p className="card music">{listened}</p>
+      <p className="card music">{favourited}</p>
+    </div>
+  );
+};
+
 const Track = () => {
-	const { loading, error, data } = useQuery(GET_SONG, {
+ const { loading, error, data } = useQuery(GET_SONG, {
     variables: {id: useParams().id},
-	});
+ });
 
   useEffect(() => {
     if (data) {
@@ -31,10 +42,10 @@ const Track = () => {
     error && console.log(error);
   }, [error]);
 
-  const MainContent =
-    loading || error ? (
-			<Spinner animation="border" />
-		) : ('track info');
+  // const MainContent =
+  //   loading || error ? (
+ //   <Spinner animation="border" />
+ //  ) : ('track info');
 
   return (
     <div className="Page Track">
@@ -43,7 +54,13 @@ const Track = () => {
           <div className="text">Track Info</div>
         </Col>
       </Col>
-      {MainContent}
+      {/* {MainContent} */}
+      <iframe className="frame" scrolling="no" frameborder="0" allowTransparency="true" src="https://www.deezer.com/plugins/player?format=square&autoplay=false&playlist=false&width=600&height=600&color=ff0000&layout=dark&size=big&type=charts&id=user_3401314024&title=%D0%95%D0%B3%D0%BE%D1%80%20%D0%A5%D0%B8%D0%BB%D1%8C%D1%87%D0%B5%D0%BD%D0%BA%D0%BE&app_id=1" width="400" height="400" />
+      <div class="buttons">
+        <p className="card music" >прослушано</p>
+        <p className="card music" >фавориты</p>
+      </div>
+      
     </div>
   );
 };
