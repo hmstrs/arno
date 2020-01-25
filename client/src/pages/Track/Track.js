c46eda7176f667abf6e9d1bb3e74030d8f63de45
Khilchenko Yehor <khilchenko.yehor@gmail.com>
2020-01-25 14:13:56 +0200

    check for id === 0

--- client/src/pages/Track/Track.js
+++ client/src/pages/Track/Track.js
@@ -6,6 +6,15 @@ import gql from "graphql-tag";
 
 import './Track.css';
 
+const frameProperties = {
+  className: "frame",
+  scrolling: "no",
+  frameBorder: "0",
+  allowtransparency: "true",
+  width: "400",
+  height: "400"
+};
+
 const GET_SONG = gql`
  query song($id: ID!) {
   getSong(id: $id) {
@@ -34,63 +43,44 @@ const CHECK_FAVOURITE = gql`
  }
 `;
 
-const createId = id => `https://www.deezer.com/plugins/player?format=square&autoplay=false&playlist=false&width=400&height=400&color=ff0000&layout=dark&size=medium&type=playlist&id=${id}&app_id=1`;
+const addToUserFavourites = async () => {
+  // make mutaion
+  // const [addToFavourites] = useMutation(ADD_TO_FAVOURITE);
+  // const newCounter = await addToFavourites({ varibles: { id: song.id } });
+  // setSong(); <- add new count of favourites
+  alert('added!');
+};
 
-
-const createFrame = (props, src) => {
+const createButtons = (listened, favourited) => {
   return (
-    <iframe {...props} src={src}/>      
+    <div className="buttons">
+      <p className="card music">listened<br/><p className="counter">{listened}</p></p>
+      <p className="card music" onClick={addToUserFavourites}>favourited<br/><p className="counter">{favourited}</p></p>
+    </div>
   );
 };
+
+const createId = id => `https://www.deezer.com/plugins/player?format=square&autoplay=false&playlist=false&width=400&height=400&color=ff0000&layout=dark&size=medium&type=playlist&id=${id}&app_id=1`;
+
+const createFrame = (props, src) => <iframe {...props} src={src}/>;
 
 const Track = () => {
 
   const [song, setSong] = useState({});
-
-  const addToUserFavourites = async () => {
-    // make mutaion
-    // const [addToFavourites] = useMutation(ADD_TO_FAVOURITE);
-    // const newCounter = await addToFavourites({ varibles: { id: song.id } });
-    // setSong(); <- add new count of favourites
-    alert('added!');
-  };
-  
-  const createButtons = (listened, favourited) => {
-    return (
-      <div className="buttons">
-        <p className="card music">listened<br/><p className="counter">{listened}</p></p>
-        <p className="card music" onClick={e => addToUserFavourites()}>favourited<br/><p className="counter">{favourited}</p></p>
-      </div>
-    );
-  };
-
-
-  const { loading, error, data } = useQuery(GET_SONG, {
-    variables: {id: useParams().id },
-  });
+  const id = useParams().id;
+  const { loading, error, data } = useQuery(GET_SONG, { variables: { id } });
 
   useEffect(() => {
-    if (data) {
-      setSong(data.getSong);
-    }
+    if (data) setSong(data.getSong);
   }, [data]);
 
   useEffect(() => {
     error && console.log(error);
   }, [error]);
-
-  const frameProperties = {
-    className: "frame",
-    scrolling: "no",
-    frameBorder: "0",
-    allowtransparency: "true",
-    width: "400",
-    height: "400"
-  };
   
-  return loading || error ? (
+  return loading ? (
     <Spinner animation="border" />
-  ) : 
+  ) : (error && id === "0") ? <p className="text track-info" margin-top="3%" >Sorry deezer can't play your song</p> :
   (
     <div className="Page Track">
       <div className="text track-info">
